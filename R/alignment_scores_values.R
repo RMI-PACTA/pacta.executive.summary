ba9df379a19f1d2fe7fa1cd0501ad8b9d1265bfc
MonikaFu <monika.furdyna@gmail.com>
2022-08-01 14:30:23 +0200

    Add scatter plot data

---
+++ R/alignment_scores_values.R
@@ -0,0 +1,10 @@
+#' Help dataset for plotting alignment scores on an axis
+#'
+#' Dataset containing numeric values corresponding to alphabetic scores for
+#' easier plotting.
+#'
+#' @keywords internal
+#'
+#' @examples
+#' pacta.executive.summary::alignment_scores_values
+"alignment_scores_values"
