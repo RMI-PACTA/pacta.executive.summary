fe1104f1b603e6272e9363a630c620d9b3e66d67
MonikaFu <monika.furdyna@gmail.com>
2022-08-02 15:20:12 +0200

    Add data scorecard

---
+++ R/toy_data_emissions_scorecard.R
@@ -0,0 +1,10 @@
+#' An example output data of `prep_emissions_scorecard()`
+#'
+#' Dataset imitating the output of `prep_emissions_scorecard()` function. Allows the
+#' user to create plots without PACTA_analysis-like input data.
+#'
+#' @family datasets
+#'
+#' @examples
+#' toy_data_emissions_scorecard
+"toy_data_emissions_scorecard"
---
+++ R/toy_data_exposures_scorecard.R
@@ -0,0 +1,10 @@
+#' An example output data of `prep_exposures_scorecard()`
+#'
+#' Dataset imitating the output of `prep_exposures_scorecard()` function. Allows the
+#' user to create plots without PACTA_analysis-like input data.
+#'
+#' @family datasets
+#'
+#' @examples
+#' toy_data_exposures_scorecard
+"toy_data_exposures_scorecard"
