6784a8a0126f08361e524446aba97690db625b4e
MonikaFu <monika.furdyna@gmail.com>
2022-08-02 11:39:07 +0200

    Add diagram data

---
+++ R/toy_data_diagram.R
@@ -0,0 +1,10 @@
+#' An example output data of `prep_diagram()`
+#'
+#' Dataset imitating the output of `prep_diagram()` function. Allows the
+#' user to create plots without PACTA_analysis-like input data.
+#'
+#' @family datasets
+#'
+#' @examples
+#' toy_data_diagram
+"toy_data_diagram"
