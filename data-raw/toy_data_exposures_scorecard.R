ebe5b6fa3542ecd0c13e74a0b7eda79f91d003a6
MonikaFu <monika.furdyna@gmail.com>
2022-08-05 15:52:14 +0200

    Fix data styling

--- data-raw/toy_data_exposures_scorecard.R
+++ data-raw/toy_data_exposures_scorecard.R
@@ -1,13 +1,15 @@
+# styler: off
 toy_data_exposures_scorecard <- tibble::tribble(
-  ~asset_class, ~sector_or_tech, ~exposure_perc_aum,
-  "equity", "coal", 0.01,
-  "equity", "other_fossil_fuels", 0.02,
-  "equity", "fossil_power", 0.15,
-  "equity", "renewables_power", 0.12,
-  "bonds", "coal", 0.03,
-  "bonds", "other_fossil_fuels", 0.01,
-  "bonds", "fossil_power", 0.1,
-  "bonds", "renewables_power", 0.2
-)
+  ~asset_class,      ~sector_or_tech, ~exposure_perc_aum,
+      "equity",               "coal",               0.01,
+      "equity", "other_fossil_fuels",               0.02,
+      "equity",       "fossil_power",               0.15,
+      "equity",   "renewables_power",               0.12,
+       "bonds",               "coal",               0.03,
+       "bonds", "other_fossil_fuels",               0.01,
+       "bonds",       "fossil_power",                0.1,
+       "bonds",   "renewables_power",                0.2
+  )
+# styler: on
 
 usethis::use_data(toy_data_exposures_scorecard, overwrite = TRUE)
