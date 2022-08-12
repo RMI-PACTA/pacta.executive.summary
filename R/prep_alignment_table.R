1d0613a36288e0feeea5d57e3a32ad295b9e702e
MonikaFu <monika.furdyna@gmail.com>
2022-08-12 17:25:10 +0200

    Add plotting functions

--- R/prep_alignment_table.R
+++ R/prep_alignment_table.R
@@ -1,3 +1,9 @@
-prep_alignment_table <- function(results_portfolio, peers_results_portfolio) {
-
+prep_alignment_table <- function(
+  results_portfolio = NULL, 
+  peers_results_aggregated = NULL, 
+  asset_class = "equity") {
+  if (is.null(results_portfolio)) {
+    data_out <- use_toy_data("alignment_table") %>% filter(asset_class == .env$asset_class)
+  }
+  data_out
 }
