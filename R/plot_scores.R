a29ff5ffb2bdf935aeafdab42578889a39ae5d0d
MonikaFu <monika.furdyna@gmail.com>
2022-08-16 11:16:05 +0200

    Add legend

--- R/plot_scores.R
+++ R/plot_scores.R
@@ -24,9 +24,9 @@ plot_scores <- function(data) {
   p_oil <- plot_score_sector(data, "oil")
   
   p_sector_up <- p_power + p_auto + p_coal + p_gas + plot_layout(ncol = 4)
-  p_sector_down <- p_steel + p_cement + p_oil + plot_spacer() + plot_layout(ncol = 4)
+  p_sector_down <- p_steel + p_cement + p_oil + legend_scores() + plot_layout(ncol = 4)
   
-  p <- p_portfolio + (p_sector_up / p_sector_down) + plot_layout(widths = c(1, 5))
+  p <- p_portfolio + (p_sector_up / p_sector_down) + plot_layout(widths = c(1, 6))
   p
 }
 
@@ -36,25 +36,25 @@ plot_score_portfolio <- function(data) {
     pull(.data$score_label)
   
   p <- plot_basic_scorebar() + 
+    annotate("segment", x = 0.5, xend = 1.5, y = c_position, yend = c_position, size = 1.3) +
     geom_text(
       data = data %>% filter(.data$entity == "this_portfolio"), 
-      x = 1.48, 
+      x = 1.5, 
       label = "\u25B2", 
       angle = 270 / pi,
-      size = 10,
+      size = 14,
       color = "#1c324f",
       aes(y = .data$score_label)
       ) +
     geom_text(
       data = data %>% filter(.data$entity != "this_portfolio"), 
-      x = 1.48, 
+      x = 1.5, 
       label = "\u25B2", 
       angle = 270 / pi,
-      size = 7,
+      size = 11,
       color = "#66696b",
       aes(y = .data$score_label)
       ) +
-    annotate("segment", x = 0.5, xend = 1.5, y = c_position, yend = c_position, size = 1.3) +
     theme(axis.line = element_blank())
   
   p
@@ -68,9 +68,17 @@ plot_score_sector <- function(data, sector) {
     pull(.data$score_label)
   
   p <- plot_basic_scorebar() + 
+    annotate(
+      "segment", 
+      x = 0.5, 
+      xend = 1.5, 
+      y = c_position, 
+      yend = c_position, 
+      size = 1
+      ) +
     geom_text(
       data = data %>% filter(.data$entity == "this_portfolio"), 
-      x = 1.48, 
+      x = 1.5, 
       label = "\u25B2", 
       angle = 270 / pi,
       size = 10,
@@ -79,20 +87,12 @@ plot_score_sector <- function(data, sector) {
       ) +
     geom_text(
       data = data %>% filter(.data$entity != "this_portfolio"), 
-      x = 1.48, 
+      x = 1.5, 
       label = "\u25B2", 
       angle = 270 / pi,
       size = 7,
       color = "#66696b",
       aes(y = .data$score_label)
-      ) +
-    annotate(
-      "segment", 
-      x = 0.5, 
-      xend = 1.5, 
-      y = c_position, 
-      yend = c_position, 
-      size = 1.5
       ) +
     theme(
       axis.line = element_blank(),
@@ -103,4 +103,62 @@ plot_score_sector <- function(data, sector) {
     )
   
   p
+}
+
+legend_scores <- function() {
+  fake_data <- tibble::tibble(
+    x = c(1, 2, 3, 3),
+    y = c(1, 2, 3, 4)
+  )
+  
+  data_labels <- tibble::tibble(
+    label = c("Portfolio", "Peers", "2\u00B0 aligned\nthreshold"),
+    x = c(1, 1, 1),
+    y = c(3.5, 2.5, 1)
+  )
+  
+  l <- ggplot(fake_data, aes(x = x, y = y)) +
+    annotate(
+      "text", 
+      x = 0,
+      y = 3.5,
+      label = "\u25B2", 
+      angle = 270 / pi,
+      size = 7,
+      color = "#1c324f"
+      ) +
+    annotate(
+      "text",
+      x = 0, 
+      y = 2.5,
+      label = "\u25B2", 
+      angle = 270 / pi,
+      size = 5,
+      color = "#66696b"
+      ) +
+    annotate(
+      "segment", 
+      x = -0.5, 
+      xend = 0.5, 
+      y = 1, 
+      yend = 1, 
+      size = 1
+      ) +
+    geom_text(
+      data = data_labels,
+      aes(
+        x = .data$x,
+        y = .data$y,
+        label = .data$label
+      ),
+      hjust = 0
+    ) +
+    scale_x_continuous(limits = c(-0.5, 3)) +
+    scale_y_continuous(limits = c(0, 4)) +
+    coord_cartesian(clip = 'off') +
+    theme_void() +
+    theme(
+      panel.spacing = unit(0, "cm")
+    )
+  l
 }
