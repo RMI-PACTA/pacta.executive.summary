30386cfee8e039d9556cb9454d6738830c8e1da1
MonikaFu <monika.furdyna@gmail.com>
2022-08-23 11:41:53 +0200

    Plot faceted

--- R/plot_scores_scorecard.R
+++ R/plot_scores_scorecard.R
@@ -1,19 +1,40 @@
 plot_scores_scorecard <- function(data) {
-  score_upper_min <- min(alignment_scores_values$score_upper)
-  score_upper_max <- max(alignment_scores_values$score_upper)
+  p <- plot_scores_scorecard_single(data) +
+    facet_wrap(~ asset_class, labeller = as_labeller(r2dii.plot::to_title))
+  p
+}
+
+plot_scores_scorecard_single <- function(data) {
+  portfolio_pointers <- unique(data$asset_class) %>% 
+    purrr::map_df(~ get_portfolio_pointer_df(data, .x) %>% mutate(asset_class = .x))
   
-  scores_labels <- c("A+", "A", "B", "C", "D", "E")
+  fill_colours_extended <- c(
+    fill_colours_scores, 
+    c("portfolio" = unname(fill_colours_entities_scores["portfolio"]))
+    )
+    
+  p <- plot_scores_pyramide() +
+    geom_polygon(
+      data = portfolio_pointers, 
+      aes(x = x, y = y, group = group, fill = group, colour = group)
+      ) +
+    scale_fill_manual(values = fill_colours_extended) +
+    scale_colour_manual(values = fill_colours_extended)
+  p
+}
+
+plot_scores_pyramide <- function() {
   c_position <- 2
   
   data_scores <- alignment_scores_values %>%
     mutate(
-      score_symbol = factor(score_symbol, levels = rev(scores_labels)),
-      y = abs(100 - score_upper + score_upper_min)
+      score_symbol = factor(score_symbol, levels = rev(scores_labels())),
+      y = calc_y_position(.data$score_upper)
     )
   
   data_triangles <- tibble::tibble(.rows = nrow(data_scores) * 3)
   idx <- 0
-  for (score in rev(scores_labels)) {
+  for (score in rev(scores_labels())) {
     idx <- idx + 1
     data_score <- data_scores %>%
       filter(
@@ -23,28 +44,11 @@ plot_scores_scorecard <- function(data) {
     data_triangles <- tibble::tibble(
       group = replicate(3, score),
       x = c(idx - 0.45, idx + 0.45, idx),
-      y = c(data_score$y, data_score$y, data_score$y + 0.05 * score_upper_max)
+      y = c(data_score$y, data_score$y, data_score$y + 0.05 * score_upper_max())
     ) %>%
     rbind(data_triangles)
   }
-    
-  score_y <- data %>%
-    dplyr::inner_join(alignment_scores_values, by = c("score" = "score_symbol")) %>%
-    pull(score_upper)
-  idx <- which(scores_labels == data$score[1])
   
-  portfolio_pointer <- tibble::tibble(
-    group = replicate(5, "portfolio"),
-    x = c(idx, idx - 0.3, idx - 0.3, idx + 0.3, idx + 0.3),
-    y = c(
-      score_y + 0.075 * score_upper_max, 
-      score_y + 0.115 * score_upper_max, 
-      score_y + 0.155 * score_upper_max, 
-      score_y + 0.155 * score_upper_max,
-      score_y + 0.115 * score_upper_max
-      )
-  )
-    
   p <- ggplot(
     data_scores,
     aes(x = score_symbol, y = y, fill = score_symbol, colour = score_symbol)
@@ -54,17 +58,13 @@ plot_scores_scorecard <- function(data) {
       data = data_triangles, 
       aes(x = x, y = y, group = group, fill = group, colour = group)
       ) +
-    geom_polygon(
-      data = portfolio_pointer, 
-      aes(x = x, y = y, group = group, fill = group, colour = group)
-      ) +
-    geom_text(aes(y = 6, label = score_symbol), colour = "black", size = 8) +
+    geom_text(aes(y = 8, label = score_symbol), colour = "black", size = 8) +
     annotate(
       "segment", 
       x = c_position + 0.5, 
       y = -1, 
       xend = c_position + 0.5, 
-      yend = score_upper_max * 1.1, 
+      yend = score_upper_max() * 1.1, 
       colour = unname(fill_colours_scores["C"]),
       size = 1.1
         ) +
@@ -81,4 +81,51 @@ plot_scores_scorecard <- function(data) {
       ) +
     coord_flip()
   p
+}
+
+annotation_df <- function() {
+  df <- tibble::tibble(
+    asset_class = c("equity", "bonds"),
+    text = c(
+      "", 
+      "Science-based\nconsensus on\nneeded global\nambition, i.e.\nScenario [XY]"
+      )
+  )
+  df
+}
+
+scores_labels <- function() {
+  c("A+", "A", "B", "C", "D", "E")
+}
+
+get_portfolio_pointer_df <- function(data, asset_class) {
+  data_asset <- data %>%
+    filter(.data$asset_class == .env$asset_class)
+  idx <- which(rev(scores_labels()) == data_asset$score[1])
+  portfolio_score <- data_asset %>%
+    dplyr::inner_join(alignment_scores_values, by = c("score" = "score_symbol")) %>%
+    pull(score_upper)
+  
+  portfolio_pointer <- tibble::tibble(
+    group = replicate(5, "portfolio"),
+    x = c(idx, idx - 0.3, idx - 0.3, idx + 0.3, idx + 0.3),
+    y = c(
+      calc_y_position(portfolio_score) + 0.075 * score_upper_max(), 
+      calc_y_position(portfolio_score) + 0.115 * score_upper_max(), 
+      calc_y_position(portfolio_score) + 0.155 * score_upper_max(), 
+      calc_y_position(portfolio_score) + 0.155 * score_upper_max(),
+      calc_y_position(portfolio_score) + 0.115 * score_upper_max()
+      )
+  )
+}
+
+score_upper_min <- function() {
+  min(alignment_scores_values$score_upper)
+}
+score_upper_max <- function() {
+  max(alignment_scores_values$score_upper)
+}
+
+calc_y_position <- function(x) {
+  abs(100 - x + score_upper_min())
 }
