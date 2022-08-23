15614d9cc8ca1b1cc813173926df980a3f9d426e
MonikaFu <monika.furdyna@gmail.com>
2022-08-23 17:40:12 +0200

    Add tests

---
+++ tests/testthat/test-plot_scores_scorecard.R
@@ -0,0 +1,38 @@
+options(warn = -1)
+
+toy_data_scores_scorecard <- function() {
+  toy_data_scores %>%
+    dplyr::filter(
+      scope == "portfolio",
+      entity == "this_portfolio"
+    )
+}
+
+test_that("if `data` is not a data frame errors gracefully", {
+  expect_snapshot_error(plot_scores_scorecard(1))
+})
+
+test_that("if `data` has zero rows errors gracefully", {
+  zero_row <- toy_data_scores_scorecard()[0L, ]
+  expect_snapshot_error(plot_scores_scorecard(zero_row))
+})
+
+test_that("with missing crucial columns errors gracefully", {
+  data_wrong_names <- toy_data_scores_scorecard()
+  colnames(data_wrong_names) <- c("bad1", "bad2", "bad3", "bad4", "bad5")
+  expect_snapshot_error(plot_scores_scorecard(data_wrong_names))
+})
+
+test_that("with wrong values of `asset_class` errors gracefully", {
+  data_wrong <- toy_data_scores_scorecard()
+  data_wrong$asset_class <- as.character(data_wrong$asset_class)
+  data_wrong$asset_class[1] <- "bad"
+  expect_snapshot_error(plot_scores_scorecard(data_wrong))
+})
+
+test_that("with wrong values of `score` errors gracefully", {
+  data_wrong <- toy_data_scores_scorecard()
+  data_wrong$score <- as.character(data_wrong$score)
+  data_wrong$score[1] <- "bad"
+  expect_snapshot_error(plot_scores_scorecard(data_wrong))
+})
