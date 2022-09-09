c06d0197f0c21129c3c51abc8b9d5c33280221b7
jacobvjk <jacob.kastl@gmail.com>
2022-09-09 11:33:21 +0200

    function to prepare portfolio files for subsequent score calculation

---
+++ R/prep_input_data_aggregate_scores.R
@@ -0,0 +1,39 @@
+prep_input_data_aggregate_scores <- function(data,
+                                             scenario_source,
+                                             scenarios,
+                                             scenario_geography,
+                                             equity_market,
+                                             start_year,
+                                             time_horizon,
+                                             green_techs) {
+  # filter data
+  # TODO: probably more filtering needed?
+  # TODO: add checks that data is given for the filter values
+  data <- data %>%
+    dplyr::filter(
+      .data$scenario_source == .env$scenario_source,
+      .data$scenario %in% .env$scenarios,
+      .data$scenario_geography == .env$scenario_geography,
+      .data$equity_market == .env$equity_market,
+      dplyr::between(.data$year, .env$start_year, .env$start_year + .env$time_horizon)
+    )
+
+  # Oil & Gas are treated as separate sectors in the aggregate score. This means
+  # the technology share must be 1 for each of them and the sector name is changed.
+
+  data <- data %>%
+    dplyr::mutate(
+      scen_tech_share = dplyr::if_else(
+        .data$ald_sector == "Oil&Gas", 1, .data$scen_tech_share
+      )
+    ) %>%
+    dplyr::mutate(
+      ald_sector = dplyr::case_when(
+        .data$technology == "Oil" ~ "Oil",
+        .data$technology == "Gas" ~ "Gas",
+        TRUE ~ .data$ald_sector
+      )
+    )
+
+  return(data)
+}
