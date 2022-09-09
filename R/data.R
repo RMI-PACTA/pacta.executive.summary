43244884c3565a06da35be601f44c173076b3899
jacobvjk <jacob.kastl@gmail.com>
2022-09-09 12:48:05 +0200

    document data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Sector level carbon budgets for scenarios
+#'
+#' A data set containing the carbon budgets by sector until 2030.
+"remaining_carbon_budgets"
+
+#' Scenario names mapped to temperature thresholds
+#'
+#' A data set containing the names of scenarios mapped to thresholds of likely
+#' climate outcomes.
+"scenario_thresholds"
