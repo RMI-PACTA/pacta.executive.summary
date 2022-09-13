1da96f7bdfea9ae59e52e1c50c14c0775df3cf4c
jacobvjk <jacob.kastl@gmail.com>
2022-09-13 15:45:08 +0200

    rm comment

--- data-raw/remaining_carbon_budgets.R
+++ data-raw/remaining_carbon_budgets.R
@@ -1,15 +1,14 @@
 # styler: off
 remaining_carbon_budgets <- tibble::tribble(
   ~scenario_source,  ~ald_sector, ~remaining_carbon_budget,           ~unit, ~weighting_factor,
-        "GECO2021",       "Coal",                     8432, "Mt CO2 (2019)",              0.24,
-        "GECO2021",        "Oil",                     8085, "Mt CO2 (2019)",              0.23,
-        "GECO2021",        "Gas",                     5702, "Mt CO2 (2019)",              0.16,
-        "GECO2021",      "Power",                     6967, "Mt CO2 (2019)",              0.19,
-        "GECO2021",      "Steel",                     1250, "Mt CO2 (2019)",              0.03,
-        "GECO2021",     "Cement",                     2455, "Mt CO2 (2019)",              0.07,
-        # TODO: split automotive in HDV and LDV
-        "GECO2021", "Automotive",                     2432, "Mt CO2 (2019)",              0.07,
-        "GECO2021",   "Aviation",                      514, "Mt CO2 (2019)",              0.01
+        "GECO2021",       "coal",                     8432, "Mt CO2 (2019)",              0.24,
+        "GECO2021",        "oil",                     8085, "Mt CO2 (2019)",              0.23,
+        "GECO2021",        "gas",                     5702, "Mt CO2 (2019)",              0.16,
+        "GECO2021",      "power",                     6967, "Mt CO2 (2019)",              0.19,
+        "GECO2021",      "steel",                     1250, "Mt CO2 (2019)",              0.03,
+        "GECO2021",     "cement",                     2455, "Mt CO2 (2019)",              0.07,
+        "GECO2021", "automotive",                     2432, "Mt CO2 (2019)",              0.07,
+        "GECO2021",   "aviation",                      514, "Mt CO2 (2019)",              0.01
   )
 # styler: on
 
