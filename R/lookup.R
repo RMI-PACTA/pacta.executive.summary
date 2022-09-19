0f4bef284d5f00326f10684d42252bc243c904bd
jacobvjk <jacob.kastl@gmail.com>
2022-09-19 21:15:34 +0200

    add indices to lookup

--- R/lookup.R
+++ R/lookup.R
@@ -8,4 +8,10 @@ techs_edge_case_aggregate_score_lookup <- c(
   "coalcap", "renewablescap", "ice", "electric", "ice_hdv", "electric_hdv"
 )
 
+index_eq_selected_lookup <- "2021Q4_iShares MSCI World UCITS ETF"
+index_eq_short_lookup <- "MSCI_world"
+
+index_cb_selected_lookup <- "2021Q4_iShares Global Corp Bond UCITS ETF"
+index_cb_short_lookup <- "iShares_Global_Corp_Bond"
+
 scatter_sector_lookup <- "Power"
