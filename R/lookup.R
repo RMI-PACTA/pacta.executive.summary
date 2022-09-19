2556c7e4c02284fdc6efaa2f10ed49b1629ef31d
jacobvjk <jacob.kastl@gmail.com>
2022-09-19 10:04:37 +0200

    lookup correct sector format

--- R/lookup.R
+++ R/lookup.R
@@ -8,4 +8,4 @@ techs_edge_case_aggregate_score_lookup <- c(
   "coalcap", "renewablescap", "ice", "electric", "ice_hdv", "electric_hdv"
 )
 
-scatter_sector_lookup <- "power"
+scatter_sector_lookup <- "Power"
