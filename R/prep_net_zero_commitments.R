729af2d544896936cc29295c8407acfc3f4da28b
jacobvjk <jacob.kastl@gmail.com>
2022-09-27 17:59:09 +0200

    skeleton of prep_net_zero_commitments

---
+++ R/prep_net_zero_commitments.R
@@ -0,0 +1,11 @@
+prep_net_zero_commitments <- function(results_portfolio,
+                                      peers_results_aggregated,
+                                      net_zero_targets) {
+  browser()
+  results_portfolio
+  peers_results_aggregated
+  net_zero_targets
+
+  # TODO: this requires isins on the peer group level. open to discuss how to best get them here
+  return(data_out)
+}
