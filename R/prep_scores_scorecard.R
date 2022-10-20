f29af18f89ead6f20488f4d7da98f98211ffb1a5
MonikaFu <monika.furdyna@gmail.com>
2022-10-20 17:51:34 +0200

    Use toy data if input is null

--- R/prep_scores_scorecard.R
+++ R/prep_scores_scorecard.R
@@ -15,29 +15,34 @@
 #' @return data.frame
 prep_scores_scorecard <- function(results_portfolio,
                                   scenario_source = "GECO2021") {
-  # create empty tibble for the peers argument
-  empty_peers_results_aggregated <- tibble::tibble()
-
-  # input checks are automatically carried out when calling prep_scores()
-  data_equity <- results_portfolio %>%
-    prep_scores(
-      peers_results_aggregated = empty_peers_results_aggregated,
-      asset_class = "equity",
-      scenario_source = scenario_source
-    ) 
-  data_bonds <- results_portfolio %>%
-    prep_scores(
-      peers_results_aggregated = empty_peers_results_aggregated,
-      asset_class = "bonds",
-      scenario_source = scenario_source
-    )
-  
-  data_out <- data_equity %>%
-    rbind(data_bonds) %>%
-    dplyr::filter(
+  if (is.null(results_portfolio)) {
+    data_out <- use_toy_data("scores") %>% filter(
       .data$scope == "portfolio",
       .data$entity == "this_portfolio"
     )
+  } else {
+    empty_peers_results_aggregated <- tibble::tibble()
 
+    # input checks are automatically carried out when calling prep_scores()
+    data_equity <- results_portfolio %>%
+      prep_scores(
+        peers_results_aggregated = empty_peers_results_aggregated,
+        asset_class = "equity",
+        scenario_source = scenario_source
+      ) 
+    data_bonds <- results_portfolio %>%
+      prep_scores(
+        peers_results_aggregated = empty_peers_results_aggregated,
+        asset_class = "bonds",
+        scenario_source = scenario_source
+      )
+    
+    data_out <- data_equity %>%
+      rbind(data_bonds) %>%
+      dplyr::filter(
+        .data$scope == "portfolio",
+        .data$entity == "this_portfolio"
+      )
+  }
   data_out
 }
