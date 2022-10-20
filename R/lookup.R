ba7080575c4ac229d40286f21e23a5c9dede2760
MonikaFu <monika.furdyna@gmail.com>
2022-10-20 16:39:11 +0200

    Adopt to prep output

--- R/lookup.R
+++ R/lookup.R
@@ -15,3 +15,5 @@ index_cb_selected_lookup <- "2021Q4_iShares Global Corp Bond UCITS ETF"
 index_cb_short_lookup <- "iShares_Global_Corp_Bond"
 
 scatter_sector_lookup <- "Power"
+
+alignment_table_temperatures_lookup <- c("<1.5C", "1.5-1.8C", ">1.8C")
