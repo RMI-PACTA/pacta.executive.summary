c83196aea41738ddee72ed52ec787920efca4026
Alex Axthelm <alex@alexaxthelm.com>
2022-11-15 16:07:21 +0100

    Don't run real estate if no real estate ressults

--- R/render_executive_summary.R
+++ R/render_executive_summary.R
@@ -38,6 +38,7 @@ render_executive_summary <- function(data,
     params = list(
       survey_dir = survey_dir,
       real_estate_dir = real_estate_dir,
+      real_estate_flag = real_estate_flag,
       language = language,
       investor_name = investor_name,
       portfolio_name = portfolio_name,
