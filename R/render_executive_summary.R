fe72a15db429b75bbff5061817d12d9d09e61be4
Jacob Kastl <60064070+jacobvjk@users.noreply.github.com>
2022-11-15 14:29:34 +0100

    read user_results from section sub directories (#93)
    * read user_results from section sub directories
    
    * update documentations

--- R/render_executive_summary.R
+++ R/render_executive_summary.R
@@ -6,6 +6,7 @@
 #' @param exec_summary_dir Character single, valid filepath to a directory that contains the template, e.g. `system.file("extdata", "PA2022CH_en_exec_summary", package = "pacta.executive.summary")`
 #' @param survey_dir Character single, valid filepath to a directory that contains the survey files for the user
 #' @param real_estate_dir Character single, valid filepath to a directory that contains real estate files for the user
+#' @param score_card_dir Character single, valid filepath to a directory that contains score card files for the user
 #' @param file_name Character single, valid filename of the Rmd template file, e.g. "template.Rmd"
 #' @param investor_name Character single string specifying the investor name
 #' @param portfolio_name Character single string specifying the portfolio name
@@ -23,6 +24,7 @@ render_executive_summary <- function(data,
                                      exec_summary_dir,
                                      survey_dir,
                                      real_estate_dir,
+                                     score_card_dir,
                                      file_name = "template.Rmd",
                                      investor_name,
                                      portfolio_name,
@@ -38,6 +40,7 @@ render_executive_summary <- function(data,
     params = list(
       survey_dir = survey_dir,
       real_estate_dir = real_estate_dir,
+      score_card_dir = score_card_dir,
       language = language,
       investor_name = investor_name,
       portfolio_name = portfolio_name,
