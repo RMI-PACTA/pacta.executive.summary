63d829e32f2b5a0709dc41b5f11396b6ae6225bf
Jackson Hoffart <jackson.hoffart@gmail.com>
2022-11-16 13:11:07 +0100

    re-export rasterGrob

---
+++ R/utils-re-exports.R
@@ -0,0 +1,13 @@
+#' Render a raster object
+#'
+#' See \code{grid::\link[grid:rasterGrob]{rasterGrob}} for details.
+#'
+#' @name rasterGrob
+#' @rdname rasterGrob
+#' @keywords internal
+#' @export
+#' @importFrom grid rasterGrob
+#' @usage rasterGrob(image)
+#' @param image Any R object that can be coerced to a raster object.
+#' @return A rastergrob grob.
+NULL
