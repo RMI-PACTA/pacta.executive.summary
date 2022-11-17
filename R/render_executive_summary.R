ebfdec8883d830281ba94712e3c54a0eda029d7d
jacobvjk <jacob.kastl@gmail.com>
2022-11-17 19:20:44 +0100

    fix log file

--- R/render_executive_summary.R
+++ R/render_executive_summary.R
@@ -15,6 +15,7 @@
 #' @param total_portfolio Data frame that contains the total portfolio as found in the standard PACTA processed inputs file "total_portfolio.rds"
 #' @param scenario_selected Character single string specifying the selected scenario, e.g. "1.5C-Unif"
 #' @param currency_exchange_value Numeric single numeric value specifying the exchange rate from USD into the desired display currency, e.g. `1.03`
+#' @param log_dir Character single, valid filepath to a directory that will contain the log file
 #'
 #' @return a pdf document written to output_dir
 #' @export
