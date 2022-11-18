e8353bd6395c4bcacaf35e6752c4a51e6fe71bea
MonikaFu <monika.furdyna@gmail.com>
2022-11-18 15:13:01 +0100

    Add colours

--- R/imports.R
+++ R/imports.R
@@ -10,5 +10,6 @@ utils::globalVariables(c(
   "fill_colours_scores",
   "axis_labels_scatter",
   "fill_colours_techmix",
-  "alignment_scores_values"
+  "alignment_scores_values",
+  "fill_colours_scores_re"
 ))
