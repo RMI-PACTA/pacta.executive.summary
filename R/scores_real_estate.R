853c83fc58285ec7ce1a4a94d0d7e6f439c8cc49
MonikaFu <monika.furdyna@gmail.com>
2022-11-18 14:27:23 +0100

    Add help dataset for scores

---
+++ R/scores_real_estate.R
@@ -0,0 +1,10 @@
+#' Help dataset for plotting real estate scores
+#'
+#' Dataset containing numeric values corresponding to alphabetic scores for
+#' easier plotting.
+#'
+#' @keywords internal
+#'
+#' @examples
+#' scores_real_estate
+"scores_real_estate"
