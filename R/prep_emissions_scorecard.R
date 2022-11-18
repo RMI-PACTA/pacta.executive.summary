5b1800f228e5fcc0ab9e516a7b94a8d97c98a819
jacobvjk <jacob.kastl@gmail.com>
2022-11-18 12:48:26 +0100

    consider all emissions in emission score card

--- R/prep_emissions_scorecard.R
+++ R/prep_emissions_scorecard.R
@@ -40,20 +40,12 @@ prep_emissions_scorecard <- function(emissions_data = NULL,
     dplyr::group_by(.data$asset_type, .data$entity) %>%
     dplyr::mutate(emissions_asset = sum(.data$weighted_sector_emissions, na.rm = TRUE)) %>%
     dplyr::ungroup() %>%
-    dplyr::mutate(
-      covered_pacta = dplyr::if_else(
-        .data$sector %in% c("Automotive", "Coal", "Oil&Gas", "Power", "Aviation", "Cement", "Steel"),
-        TRUE,
-        FALSE
-      )
-    ) %>%
-    dplyr::group_by(.data$asset_type, .data$entity, .data$covered_pacta, .data$emissions_asset) %>%
+    dplyr::group_by(.data$asset_type, .data$entity, .data$emissions_asset) %>%
     dplyr::summarise(
       emissions = sum(.data$weighted_sector_emissions, na.rm = TRUE),
       .groups = "drop"
     ) %>%
     dplyr::ungroup() %>%
-    dplyr::filter(.data$covered_pacta) %>%
     dplyr::select(c("asset_type", "entity", "emissions")) %>%
     dplyr::rename(asset_class = "asset_type") %>%
     dplyr::mutate(asset_class = tolower(.data$asset_class))
