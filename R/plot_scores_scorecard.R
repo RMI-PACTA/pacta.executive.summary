cc6fa5038c09ac009f5ff79211d45ccd5db7d5ed
Monika Furdyna <monika.furdyna@gmail.com>
2022-11-19 23:46:12 +0100

    Update scores annotation (#170)
    * Adjust sizes scores scorecard
    
    * Set real estate fig size
    
    * Adjust sizes bar plots
    
    * Update scores annotation
    
    * use unicode character

--- R/plot_scores_scorecard.R
+++ R/plot_scores_scorecard.R
@@ -151,7 +151,7 @@ plot_scores_pyramide <- function(
 
 annotation_df <- function(data) {
   nr_assets <- length(unique(data$asset_class))
-  annotation_text <-  "Science-based\nconsensus on\nneeded glo-\nbal ambition,\ni.e. Scenario\nNDC-LTS"
+  annotation_text <-  "Minimum\nParis\nAgreement\ntemperature\ngoal (1.8\u00B0C\nscenario)"
   if ( nr_assets == 2) {
     df <- tibble::tibble(
       asset_class = c("bonds", "equity"),
