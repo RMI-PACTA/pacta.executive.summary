425f4967d7b406c1ffcf96f880201d4865c1df85
Monika Furdyna <monika.furdyna@gmail.com>
2024-05-07 22:40:15 +0200

    Update scenario thresholds (#308)

--- data-raw/scenario_thresholds.R
+++ data-raw/scenario_thresholds.R
@@ -3,7 +3,10 @@ scenario_thresholds <- tibble::tribble(
   ~scenario_source,   ~scenario, ~threshold,
   "GECO2021",       "1.5C-Unif",     "high",
   "GECO2021",         "NDC-LTS",      "mid",
-  "GECO2021",          "CurPol",      "low"
+  "GECO2021",          "CurPol",      "low",
+  "GECO2023",            "1.5C",     "high",
+  "GECO2023",         "NDC-LTS",      "mid",
+  "GECO2023",       "Reference",      "low"
 )
 # styler: on
 
