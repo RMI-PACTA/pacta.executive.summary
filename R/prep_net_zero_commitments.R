6f0c2c02bd49aef659907f4fe19599bedf24dab9
Antoine LALECHERE <alalechere@rmi.org>
2024-06-19 17:35:50 +0200

    add second way to compute net zero commitment

--- R/prep_net_zero_commitments.R
+++ R/prep_net_zero_commitments.R
@@ -16,12 +16,18 @@
 prep_net_zero_commitments <- function(total_portfolio,
                                       peer_group = c("pensionfund", "assetmanager", "bank", "insurance", "other"),
                                       net_zero_targets,
+                                      peer_group_company_number_net_zero,
                                       peer_group_share_net_zero) {
   # match input arg
   peer_group <- match.arg(peer_group)
 
   # calculate portfolio level share of sbti commitments
-  portfolio_share_net_zero <- prep_portfolio_sbti_commitments(
+  portfolio_number_company_net_zero <- prep_portfolio_sbti_commitments(
+    total_portfolio = total_portfolio,
+    net_zero_targets = net_zero_targets
+  )
+
+  portfolio_portfolio_share_net_zero <- prep_portfolio_sbti_compliant_commitments(
     total_portfolio = total_portfolio,
     net_zero_targets = net_zero_targets
   )
@@ -30,17 +36,26 @@ prep_net_zero_commitments <- function(total_portfolio,
   peer_group_share_net_zero <- peer_group_share_net_zero %>%
     dplyr::filter(.data$investor_name == .env$peer_group)
 
+  peer_group_company_number_net_zero <- peer_group_company_number_net_zero %>%
+    dplyr::filter(.data$investor_name == .env$peer_group)
+
   # combin portfolio and peer level results
-  data_out <- portfolio_share_net_zero %>%
-    dplyr::bind_rows(peer_group_share_net_zero)
+  data_out_number_company <- portfolio_number_company_net_zero %>%
+    dplyr::bind_rows(peer_group_company_number_net_zero) %>%
+    dplyr::mutate(indicator = "number_company")
+
+  data_out_share_portfolio <- portfolio_portfolio_share_net_zero %>%
+    dplyr::bind_rows(peer_group_share_net_zero) %>%
+    dplyr::mutate(indicator = "exposure_share")
+
+  data_out <- data_out_number_company %>%
+    dplyr::bind_rows(data_out_share_portfolio)
 
   return(data_out)
 }
 
 prep_portfolio_sbti_commitments <- function(total_portfolio,
                                             net_zero_targets) {
-  net_zero_targets <- net_zero_targets %>%
-    dplyr::mutate(has_net_zero_commitment = TRUE)
 
   portfolio_sbti_commitments <- total_portfolio %>%
     dplyr::mutate(
@@ -60,4 +75,30 @@ prep_portfolio_sbti_commitments <- function(total_portfolio,
     dplyr::ungroup()
 
   return(portfolio_sbti_commitments)
+}
+
+prep_portfolio_sbti_compliant_commitments <- function(total_portfolio,
+                                                      net_zero_targets) {
+  portfolio_sbti_compliant_commitments <- total_portfolio %>%
+    dplyr::mutate(
+      investor_name = "this_portfolio",
+      portfolio_name = "this_portfolio"
+    ) %>%
+    dplyr::left_join(
+      net_zero_targets,
+      by = c("isin", "factset_entity_id")
+    )  %>%
+    dplyr::filter(asset_type %in% c("Bonds", "Equity")) %>%
+    mutate(
+      usd_value_net_zero_commitment = ifelse(.data$has_net_zero_commitment,
+                                       .data$value_usd,
+                                       0
+      )
+    )  %>%
+    dplyr::group_by(.data$investor_name, .data$portfolio_name) %>%
+    dplyr::summarise(
+      share_net_zero = sum(.data$usd_value_net_zero_commitment, na.rm = TRUE) / sum(.data$value_usd, na.rm = TRUE),
+      .groups = "drop"
+    ) %>%
+    dplyr::ungroup()
 }
