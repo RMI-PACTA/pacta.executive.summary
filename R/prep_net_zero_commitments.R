fd8202f15881511160a6231bea4634c1e7b5bc1c
Antoine-Lalechere <73108425+Antoine-Lalechere@users.noreply.github.com>
2024-06-27 16:53:08 +0200

    338 align nz commitment sbti (#340)
    * prepare net zero financial data from SBTI data
    
    * change column name to adapt to next step of the workflow
    
    * add second way to compute net zero commitment
    
    * add one variable for the junction
    
    * remove proprietary pathway
    
    * add peer group results
    
    * add documentation
    
    * add binding
    
    * remove file that should go to workflow.prep.PA2024CH
    
    * group peer results in one function and one output
    
    * name more explicite
    
    * change in output formatting
    
    * standardize output by renaming peergroup once filtered
    
    * replace net_zero_targets by fin_data_net_zero_targets
    
    * add documentation
    
    * add documentation
    
    * remove typo
    
    * add dependencies
    
    * change fin_data_net_zero_targets
    
    * update Rproj
    
    * remove prepare_peer_group_net_zero_commitment
    
    * Update pacta.executive.summary.Rproj
    
    Co-authored-by: Alex Axthelm <aaxthelm@rmi.org>
    
    * update documentation
    
    ---------
    
    Co-authored-by: Alex Axthelm <aaxthelm@rmi.org>
    Co-authored-by: Monika Furdyna <monika.furdyna@gmail.com>

--- R/prep_net_zero_commitments.R
+++ R/prep_net_zero_commitments.R
@@ -6,41 +6,52 @@
 #' @param total_portfolio Data frame. Contains processed input of the PACTA for
 #'   Investors calculation.
 #' @param peer_group Character. Peer group of the analysed portfolio.
-#' @param net_zero_targets Data frame. Contains information on which ISINs
+#' @param fin_data_net_zero_targets Data frame. Contains information on which ISINs
 #'   belong to companies that have committed to SBTI net zero targets.
-#' @param peer_group_share_net_zero Data frame. Contains precalculated shares of
-#'   companies in the peer group portfolios that have SBTI net zero commitments.
+#' @param peers_net_zero_commitment DataFrame. Contains informations on results at peer group level to be compared with portfolio
 #'
 #' @return data.frame
 #' @export
 prep_net_zero_commitments <- function(total_portfolio,
                                       peer_group = c("pensionfund", "assetmanager", "bank", "insurance", "other"),
-                                      net_zero_targets,
-                                      peer_group_share_net_zero) {
+                                      fin_data_net_zero_targets,
+                                      peers_net_zero_commitment
+                                      ) {
   # match input arg
   peer_group <- match.arg(peer_group)
 
   # calculate portfolio level share of sbti commitments
-  portfolio_share_net_zero <- prep_portfolio_sbti_commitments(
+  portfolio_number_company_net_zero <- prep_portfolio_sbti_commitments(
     total_portfolio = total_portfolio,
-    net_zero_targets = net_zero_targets
+    fin_data_net_zero_targets = fin_data_net_zero_targets
+  )
+
+  portfolio_portfolio_share_net_zero <- prep_portfolio_sbti_compliant_commitments(
+    total_portfolio = total_portfolio,
+    fin_data_net_zero_targets = fin_data_net_zero_targets
   )
 
   # get peers sbti commitment for appropriate peer group
-  peer_group_share_net_zero <- peer_group_share_net_zero %>%
-    dplyr::filter(.data$investor_name == .env$peer_group)
+  peers_net_zero_commitment <- peers_net_zero_commitment %>%
+    dplyr::filter(.data$investor_name == .env$peer_group)  %>%
+    dplyr::mutate(investor_name = "peergroup",
+           portfolio_name = "peergroup")
 
-  # combin portfolio and peer level results
-  data_out <- portfolio_share_net_zero %>%
-    dplyr::bind_rows(peer_group_share_net_zero)
+  # combine portfolio and peer level results
+  data_out <- portfolio_number_company_net_zero %>%
+    dplyr::left_join(portfolio_portfolio_share_net_zero, by = c("investor_name", "portfolio_name")) %>%
+    dplyr::bind_rows(peers_net_zero_commitment) %>%
+    tidyr::pivot_longer(cols = c("company_share_net_zero", "exposure_share_net_zero")) %>%
+    dplyr::select(-"investor_name") %>%
+    tidyr::pivot_wider(
+      names_from ="portfolio_name"
+    )
 
   return(data_out)
 }
 
 prep_portfolio_sbti_commitments <- function(total_portfolio,
-                                            net_zero_targets) {
-  net_zero_targets <- net_zero_targets %>%
-    dplyr::mutate(has_net_zero_commitment = TRUE)
+                                            fin_data_net_zero_targets) {
 
   portfolio_sbti_commitments <- total_portfolio %>%
     dplyr::mutate(
@@ -48,16 +59,42 @@ prep_portfolio_sbti_commitments <- function(total_portfolio,
       portfolio_name = "this_portfolio"
     ) %>%
     dplyr::left_join(
-      net_zero_targets,
+      fin_data_net_zero_targets,
       by = c("isin", "factset_entity_id")
     ) %>%
     dplyr::distinct(.data$investor_name, .data$portfolio_name, .data$factset_entity_id, .data$has_net_zero_commitment) %>%
     dplyr::group_by(.data$investor_name, .data$portfolio_name) %>%
     dplyr::summarise(
-      share_net_zero = sum(.data$has_net_zero_commitment, na.rm = TRUE) / dplyr::n(),
+      company_share_net_zero = sum(.data$has_net_zero_commitment, na.rm = TRUE) / dplyr::n(),
       .groups = "drop"
     ) %>%
     dplyr::ungroup()
 
   return(portfolio_sbti_commitments)
+}
+
+prep_portfolio_sbti_compliant_commitments <- function(total_portfolio,
+                                                      fin_data_net_zero_targets) {
+  portfolio_sbti_compliant_commitments <- total_portfolio %>%
+    dplyr::mutate(
+      investor_name = "this_portfolio",
+      portfolio_name = "this_portfolio"
+    ) %>%
+    dplyr::left_join(
+      fin_data_net_zero_targets,
+      by = c("isin", "factset_entity_id", "asset_type")
+    )  %>%
+    dplyr::filter(.data$asset_type %in% c("Bonds", "Equity")) %>%
+    mutate(
+      usd_value_net_zero_commitment = ifelse(.data$has_net_zero_commitment,
+                                       .data$value_usd,
+                                       0
+      )
+    )  %>%
+    dplyr::group_by(.data$investor_name, .data$portfolio_name) %>%
+    dplyr::summarise(
+      exposure_share_net_zero = sum(.data$usd_value_net_zero_commitment, na.rm = TRUE) / sum(.data$value_usd, na.rm = TRUE),
+      .groups = "drop"
+    ) %>%
+    dplyr::ungroup()
 }
